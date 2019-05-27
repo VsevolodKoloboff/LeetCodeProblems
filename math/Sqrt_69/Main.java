4fe8837cde145af686f7f470ff8dce06f82ba548
Vsevolod Kolobov <vsevolok.atribut@gmail.com>
2019-05-27 21:09:58 +0300

    added problem-solution 69

---
+++ math/Sqrt_69/Main.java
@@ -0,0 +1,16 @@
+package Sqrt_69;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Solution solution =new Solution();
+        System.out.println(solution.mySqrt(-1));
+        System.out.println(solution.mySqrt(4));
+        System.out.println(solution.mySqrt(124));
+        System.out.println(solution.mySqrt(1));
+        System.out.println(solution.mySqrt(0));
+
+
+
+    }
+}
