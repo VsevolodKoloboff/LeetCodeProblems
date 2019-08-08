9604e8f7728f23a6b48d8ba8b308efbbe30f2caa
Vsevolod Kolobov <vsevolok.atribut@gmail.com>
2019-08-08 03:03:06 +0300

    fix 717

--- easy/tasks/OnebitandTwobitCharacters_717.java
+++ easy/tasks/OnebitandTwobitCharacters_717.java
@@ -8,10 +8,20 @@ public class OnebitandTwobitCharacters_717 {
      * Return whether the last character must be a one-bit character or not.
      * The given string will always end with a zero.*/
 
-    public boolean isOneBitCharacter(int[] bits) {
-        int i = 0;
-        while (i<bits.length-1)
-            i+=bits[i]+1;
+    public static boolean isOneBitCharacter(int[] bits) {
+
+        int i = bits.length - 2;
+        while (i >= 0 && bits[i] > 0)
+            i--;
+
         return (bits.length - i) % 2 == 0;
+    }
+    /*main*/
+    public static void main(String[] args) {
+        int arr1[] = {1, 0, 1, 1};
+        System.out.println(isOneBitCharacter(arr1));
+        int arr2[] = {1, 0, 0};
+        System.out.println(isOneBitCharacter(arr2));
+
     }
 }
